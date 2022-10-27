a06c980d10d5f71aa119807b5466345127c867d4
秦锋 <iamzhoufei@gmail.com>
2022-10-27 22:03:11 +0800

    update: confirm homepage's structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,71 @@
+import { Button, TextField } from '@mui/material'
+import { SvgGoogle, SvgLock } from '../components/icons'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './index.module.scss'
+import InfoCardComponent from '../components/InfoCard'
+
+type TCard = {
+  id: string;
+  backgroundImage: string;
+  time: string;
+  weather: string;
+}
 
 export default function Home() {
 
+  const [cards, setCards] = useState<TCard[]>([]);
+
+  useEffect(() => {
+    setCards([
+      {
+        id: 'bear',
+        backgroundImage: '',
+        time: '',
+        weather: ''
+      },
+      {
+        id: 'rabbit',
+        backgroundImage: '',
+        time: '',
+        weather: ''
+      }
+    ])
+  }, [])
+
   return (
     <div className={styles.container}>
-      Home
-      <Link href='/detail'>按钮</Link>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <div className={styles.cards}>
+            <InfoCardComponent />
+            {/* <SvgLock /> */}
+            <InfoCardComponent />
+          </div>
+        
+          <div className={ styles.utils}></div>
+        </div>
+        <div className={styles.right}>
+          <TextField
+            fullWidth
+            label=""
+            placeholder={`Search Google`}
+            InputProps={{
+              endAdornment: <SvgGoogle style={{ fontSize: '20px' }} />,
+            }}
+          />
+
+
+          <Link href='/detail'>
+            <Button variant="contained">Hello World</Button>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.footer}>
+
+      </div>
     </div>
   )
 }
