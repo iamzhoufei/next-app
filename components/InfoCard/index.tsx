6d410b4b350210fb218a43f0608a59c5fed06723
秦锋 <iamzhoufei@gmail.com>
2022-10-29 22:21:32 +0800

    update: develop InfoCard component

--- components/InfoCard/index.tsx
+++ components/InfoCard/index.tsx
@@ -1,5 +1,28 @@
+import { Card, Avatar } from 'antd';
+import { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 
-export default function InfoCardComponent(props: any) {
-    return <div className={styles.card}></div>
+const { Meta } = Card;
+
+type TInfoCard = {
+    id: string;
+    weather: string;
+    time: string;
+};
+
+export default function InfoCardComponent(props: TInfoCard) {
+
+    const [loading, setLoading] = useState(!props.id);
+
+    useEffect(() => {
+        if (props.id) setLoading(false)
+    }, [props.id])
+
+    return <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
+        <Meta
+            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+            title="Card title"
+            description="This is the description"
+        />
+    </Card>
 }
