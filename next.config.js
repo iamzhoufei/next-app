4e087e431cdcba016fdef52e15e989e4c054cb76
秦锋 <iamzhoufei@gmail.com>
2022-10-29 22:19:10 +0800

    chore: change UI frame & support svg component

--- next.config.js
+++ next.config.js
@@ -8,6 +8,28 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'babel-loader',
+        },
+        {
+          loader: '@svgr/webpack',
+          options: {
+            babel: false,
+            icon: true,
+          },
+        },
+      ],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
